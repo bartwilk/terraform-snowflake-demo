4787049da8c3b91c9671b166b4c6bf3209dc29f0
bartwilk <bartwilk@gmail.com>
2025-10-29 07:29:12 -0400

    Updated Snowflake private key and passphrase.

--- prod/main.tf
+++ prod/main.tf
@@ -2,13 +2,13 @@ terraform {
   required_providers {
     snowflake = {
       source  = "Snowflake-Labs/snowflake"
-      version = "0.63.0"
+      version = "0.66.2"
     }
   }
 
   backend "gcs" {
-    bucket = "tf-state-bucket10001"   
-    prefix = "prod"                   # state stored at gs://<bucket>/prod/<workspace>.tfstate
+    bucket = "tf-state-bucket10001"
+    prefix = "prod" # state stored at gs://<bucket>/prod/<workspace>.tfstate
     # optional:
     # impersonate_service_account = "terraform@YOUR_PROJECT_ID.iam.gserviceaccount.com"
     # encryption_key             = "BASE64_32_BYTE_CUSTOMER_SUPPLIED_KEY"
@@ -16,10 +16,11 @@ terraform {
 }
 
 provider "snowflake" {
-  username    = "BARTWILK"
-  account     = "BMPXPNG-PDB39891"
-  role        = "ACCOUNTADMIN"
-  private_key = var.snowflake_private_key
+  username               = "BARTWILK"
+  account                = "BMPXPNG-PDB39891"
+  role                   = "ACCOUNTADMIN"
+  private_key            = file("../sf_rsa_key.p8")
+  private_key_passphrase = file("../passphrase.tfvars")
 }
 
 module "snowflake_resources" {
